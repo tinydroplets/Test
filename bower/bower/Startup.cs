29f8966759f00d2faf642d419e93f68ecd1d74d4
tinydroplets <abdulla.github@gmail.com>
2016-09-25 21:07:37 -0500

    having some fun with bower

---
+++ bower/bower/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bower.Startup))]
+namespace bower
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
