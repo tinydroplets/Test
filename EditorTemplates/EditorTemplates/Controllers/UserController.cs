fd548c5addf8f03edf7effefa9d40554dca4a647
tinydroplets <abdulla.github@gmail.com>
2016-09-25 19:50:12 -0500

    editor template for user

---
+++ EditorTemplates/EditorTemplates/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace EditorTemplates.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
