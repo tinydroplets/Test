81479909e7fc2de0f4b503cbea909b5899498598
tinydroplets <abdulla.github@gmail.com>
2016-10-19 08:36:39 -0500

    looks like an initial check-in but some code was already written.

--- ExpressMapper/ExpressMapper/Program.cs
+++ ExpressMapper/ExpressMapper/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using ExpressMapper.Extensions;
 
 namespace ExpressMapper
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            //simple mapping to EmployeeViewModel
+            Mapper.Register<Employee, EmployeeViewModel>();
+            var employee = new Employee() {Id = 1, Name = "Fido"};
+            var evm = employee.Map<Employee, EmployeeViewModel>();
         }
     }
 }
